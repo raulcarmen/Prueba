5eae5ed04c6812cf0c9223d07c834df2ed7f5b88
raulcarmen <79952391+raulcarmen@users.noreply.github.com>
2021-03-03 22:26:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import machine, time, random 
+import machine, time 
 led = machine.Pin(2, machine.Pin.OUT)
+n = 1
+while n <= 1000000:
+	led.on()
+	time.sleep(0.5)
+	c = 1
+	x = 0
+	while c <= n:
+		if n%c == 0:
+			x = x + 1
 
-r = int(random.randrange(l000000))
-cont = 0
-print()
-print("{0} es divisible por".format(r), end=": ")
-for n in range(1, r+1):
-  if r % n == 0:
-    print(n, end=" - ")
-    cont += 1
-print("Fin")
-if cont == 2:
-  print("El numero ingresado si es primo, tiene {0} divisores".format(cont))
-  led.off()
-else:
-  print("El numero ingresado no es primo, tiene {0} divisores".format(cont))
-  led.on()	
+		c = c + 1
+	if x==2:
+		print (n)
+		print("Es un numero primo")
+		led.off()
+		time.sleep(0.5)
+	n = n + 1	
 		
